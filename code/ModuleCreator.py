e34962fd4c4ed34efa9a64b6aa2bd9f3b7574fe2
KuluAndrej <kulu-andrej@yandex.ru>
2016-03-28 21:38:20 +0300

    Clean and restructurize the project

--- code/ModuleCreator.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-
-setup(
-    ext_modules=[
-        Extension("mutator", ["Mutator.cpp"],
-        libraries = ["boost_python"])
-    ])
