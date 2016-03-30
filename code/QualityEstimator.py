2e20b09357565b265c38dabee025eebc0678ed8b
KuluAndrej <kulu-andrej@yandex.ru>
2016-03-30 15:12:12 +0300

    Add quality evaluation

---
+++ code/QualityEstimator.py
@@ -0,0 +1,16 @@
+from code.structures.Model import Model
+
+def strings_to_population(handles):
+    """
+    Get a list of superposition handles and return a list of instances of Model-class
+    Inputs:
+     handles        - list of superposition handles
+
+    Outputs:
+     populations    - list of instances of Model-class built from handles
+
+    """
+    population = list(map(Model, handles))
+
+    return population
+
